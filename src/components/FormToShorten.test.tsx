a1640c7dddc4576443427b3a76fb325f7f2d2a35
carlosqdev <carlosquintero95nic@gmail.com>
2023-02-06 21:01:52 -0600

    Test: testing to component FormToShorten

---
+++ src/components/FormToShorten.test.tsx
@@ -0,0 +1,48 @@
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import { render, screen, cleanup, fireEvent } from '@testing-library/react'
+import { Provider } from 'react-redux'
+
+import { FormToShorten } from './FormToShorten'
+import { getShortenLink } from '../services/get-shorten-link'
+import { store } from '../context/app/store'
+
+
+describe('FormToShorten', () => {
+	afterEach(cleanup)
+
+	it('Should not call the api without a link', () => {
+		const mockFn = vi.fn()
+
+		render(<Provider store={store}>
+			<FormToShorten shorteLink={mockFn}/>
+		</Provider>)
+
+
+		const button = screen.getByRole('button', {
+			name: /shorten it!/i
+		})
+
+		fireEvent.click(button)
+
+		expect(mockFn).not.toBeCalled()
+	})
+
+	it('Should show alert to user when input is empty', () => {
+		render(<Provider store={store}>
+			<FormToShorten shorteLink={getShortenLink}/>
+		</Provider>)
+
+		const button = screen.getByRole('button', {
+			name: /shorten it!/i
+		})
+
+		fireEvent.click(button)
+
+		setTimeout(() => {
+			const span = screen.getByText(/please add a link/i)
+
+			expect(span).toBeDefined()
+		}, 5)
+	})
+
+})
