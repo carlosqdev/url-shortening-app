712a7c6670e6dab644fa1d818f345564d0c28faf
carlosqdev <carlosquintero95nic@gmail.com>
2023-02-09 15:08:53 -0600

    refactor Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,14 +7,18 @@ export const Header = () => {
   return (
     <header className='header'>
       <div className='header_wrapper'>
-        <img className='header_logo' aria-label='shortly logo' src={ShortlyLogo} alt="Shortly logo" />
+        <figure>
+          <img className='header_logo' aria-label='shortly logo' src={ShortlyLogo} alt="Shortly logo" />
+        </figure>
 
         <input className='header_expand_menu' aria-label='expand-menu' type="checkbox" name="expand-menu" id="expand-menu" />
 
         <Navbar />
 
         <label className='header_label' htmlFor='expand-menu'>
-          <img className='header_hamburguer_image' aria-label='hamburguer menu' src={HamburguerMenu} alt="hamburguer menu" />
+          <figure>
+            <img className='header_hamburguer_image' aria-label='hamburguer menu' src={HamburguerMenu} alt="hamburguer menu" />
+          </figure>
         </label>
       </div>
     </header>
