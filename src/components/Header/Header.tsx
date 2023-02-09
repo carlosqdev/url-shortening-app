cc97963342476ec28c1f7250f158b8a2b34b767f
carlosqdev <carlosquintero95nic@gmail.com>
2023-02-09 10:00:49 -0600

    Styles component Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,17 @@
 import ShortlyLogo from '../../assets/images/logo.svg'
 import HamburguerMenu from '../../assets/images/hamburguer_menu.svg'
+import './Header.css'
 
 export const Header = () => {
   return (
-    <header>
-      <img aria-label='shortly logo' src={ShortlyLogo} alt="Shortly logo" />
+    <header className='header'>
+      <div className='header_wrapper'>
+        <img className='header_logo' aria-label='shortly logo' src={ShortlyLogo} alt="Shortly logo" />
 
-      <button>
-        <img aria-label='hamburguer menu' src={HamburguerMenu} alt="hamburguer menu" />
-      </button>
+        <button className='header_button_hamburguer'>
+          <img className='header_hamburguer_image' aria-label='hamburguer menu' src={HamburguerMenu} alt="hamburguer menu" />
+        </button>
+      </div>
     </header>
   )
 }
