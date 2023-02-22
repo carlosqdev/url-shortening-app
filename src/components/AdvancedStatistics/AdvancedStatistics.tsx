17d1fb6128611072ff85a6bd2c3fedb8ea44da1d
carlosqdev <carlosquintero95nic@gmail.com>
2023-02-22 15:43:38 -0600

    feat: new component with styles

---
+++ src/components/AdvancedStatistics/AdvancedStatistics.tsx
@@ -0,0 +1,15 @@
+import './AdvancedStatistics.css'
+
+export const AdvancedStatistics = () => {
+  return (
+    <section className='advancedStatistics'>
+      <header>
+        <h2 className='advancedStatistics_title'>Advanced Statistics</h2>
+        <p role='paragraph'>
+          Track how your links are performing across the web with our advanced
+          statistics dashboard.
+        </p>
+      </header>
+    </section>
+  )
+}
