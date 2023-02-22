010960e8cc7139eeaa97f4f52594075ee79416a5
carlosqdev <carlosquintero95nic@gmail.com>
2023-02-22 15:42:56 -0600

    Test: AdvancedStatistics

---
+++ src/components/AdvancedStatistics/AdvancedStatistics.test.tsx
@@ -0,0 +1,31 @@
+import { describe, it, expect, afterEach } from 'vitest'
+import { cleanup, render, screen } from '@testing-library/react'
+import { AdvancedStatistics } from './AdvancedStatistics'
+
+describe('AdvancedStatistics', () => {
+  afterEach(cleanup)
+
+  it('should render', () => {
+    render(<AdvancedStatistics />)
+
+    expect(<AdvancedStatistics />).toBeDefined()
+  })
+
+  it('should render a title with text Advanced Statistics', () => {
+    render(<AdvancedStatistics />)
+
+    const title = screen.getByRole('heading', {
+      name: /Advanced Statistics/
+    })
+
+    expect(title).toBeDefined()
+  })
+
+  it('should have a paragraph with more description', () => {
+    render(<AdvancedStatistics />)
+
+    const paragraph = screen.getByRole('paragraph')
+
+    expect(paragraph).toBeDefined()
+  })
+})
